f1f85b8339b02bb63514a4fa2c44634732ec2688
Linus <zerolecture0922@gmail.com>
2022-03-28 23:16:30 +0900

    Feat : Kotlin Basics 강의에 담긴 모든 실습 완료

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,6 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import screen.ShoppingHome
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    val shoppingHome = ShoppingHome()
+    shoppingHome.start()
 }
